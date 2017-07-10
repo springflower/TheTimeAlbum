9a659c16dfeb750a4a2caa56c8732638ffe086e0
林子涵 <springflower9999@gmail.com>
2017-07-10 16:58:32 +0800

    Initial Commit

---
+++ TheTimeAlbum/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TheTimeAlbum
+//
+//  Created by 林子涵 on 2017/7/10.
+//  Copyright © 2017年 Greathard. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
